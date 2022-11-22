70d723dcd7020d677f7e50a48d49210a539ea708
LupoArticoblu <wolf94-@libero.it>
2022-11-22 16:12:22 +0100

    vite-first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- app vue -->
+
+<script>
+import FirstComponent from "./components/FirstComponent.vue";
+export default {
+  components: { FirstComponent },
+  data() {
+    return {
+      text: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ text }}</h1>
+  <first-component />
+</template>
+
+<style></style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,22 @@
+<!-- app vue -->
+
+<script>
+export default {
+  name: "FirstComponent",
+  data() {
+    return {
+      text: "Sono il primo componente!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ text }}</h2>
+</template>
+
+<style scoped>
+h2 {
+  color: blue;
+}
+</style>
